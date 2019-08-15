ab2177d24f6609c68da41435683ab2cbc363ccac
Peter Chacko <workpraytoday@gmail.com>
2019-08-15 16:02:21 -0500

    Remove extraneous imports. Generate .tex files

--- proto.py
+++ proto.py
@@ -1,12 +1,12 @@
-import numpy as np
+import gnureadline
 
+import datetime
+import os
 
 
 from pylatex import Document, Section, Subsection, Subsubsection, Command, Itemize
 from pylatex.section import Paragraph
 from pylatex.utils import italic, NoEscape
-import datetime
-import os
 
 def report(doc, subject="Project"):
     project = input(subject + ": ")
@@ -14,21 +14,35 @@ def report(doc, subject="Project"):
     report = input("Report: ")
     with doc.create(Subsubsection(project + " -- " + person)):
         doc.append(report)
-        
+
+def smart_input(prompt, default=""):
+    user_input = input(prompt)
+    if not default:
+        return user_input
+    else:
+        return user_input or default
+
+def askUser(doc, subject, bullets=False):
+    subject_section = input("Any " + subject + "s? ")
+    if not bullets:
+        while subject_section == 'y':
+            report(doc, subject=subject)
+            subject_section = input("More " + subject + "s? ")
+    else:
+        if subject_section == 'y':
+            with doc.create(Itemize()) as itemize:
+                while subject_section == 'y':
+                    subject_bullet = input(subject + ": ")
+                    itemize.add_item(str(subject_bullet))
+                    subject_section = input("More " + subject + "s? ")
+                itemize.append(Command("ldots"))
 
 def announcements(doc):
-    with doc.create(Itemize()) as itemize:
-        announcement = input("Annoucement Section: ")
-        if announcement == 'y':
-            while announcement == 'y':
-                new_announcement = input("Announcement: ")
-                itemize.add_item(str(new_announcement))
-                announcement = input("More announcements: ")
-            itemize.append(Command("ldots"))
-
+    askUser(doc, "Announcement", bullets=True)
+    pass
 def add_business(doc):
     business = input("Business on agenda: ")
-    
+
     with doc.create(Subsubsection(business)):
         pass
 
@@ -37,9 +51,9 @@ def run_business(doc, type_of_business="Old Business"):
     while business_section == 'y':
         add_business(doc)
         move_section = input("More motions? ")
-        
+
         with doc.create(Itemize()) as itemize:
-        
+
             while move_section == 'y':
                 itemize.add_item(move(doc))
                 move_section = input("More motions? ")
@@ -63,9 +77,9 @@ def move(doc):
             amended = True
             amendedMotion = input("Amended Motion: ")
             amendedMover = input("Who moved to amend motion? ")
-            
+
         vote = input("Yay (y) or nay (n): ")
-        writeup = "%s moved to %s. %s seconded the motion." % (person, motion, second) 
+        writeup = "%s moved to %s. %s seconded the motion." % (person, motion, second)
         if amended:
             writeup += " After discussion, %s moved to amend the motion to %s. It was seconded." % (amendedMover, amendedMotion)
         if vote == 'y':
@@ -75,34 +89,34 @@ def move(doc):
         return writeup
 
 if __name__ == '__main__':
-    members = ["Hope Berry", "Shepard Berry", "Peter Chacko", "Thomas Chacko", "Isabel Bettencourt", "Grace Bettencourt", "Claire Bettencourt", "Sara Bettencourt"]
+    members = ["Hope Berry", "Shepard Berry", "Peter Chacko", "Thomas Chacko", "Lauren Bettencourt",  "Isabel Bettencourt", "Grace Bettencourt", "Claire Bettencourt", "Sara Bettencourt"]
     doc = Document(datetime.date.today().strftime("%B %d, %Y") + " 4-H Business Meeting")
     doc.preamble.append(Command('title', r'4H Business Meeting'))
     doc.preamble.append(Command('author', r'Peter Chacko'))
     doc.preamble.append(Command('date', NoEscape(r'\today')))
     doc.append(NoEscape(r'\maketitle'))
-    
-    current_time = str(datetime.datetime.now())        
+
+    current_time = str(datetime.datetime.now())
     # Call to Order
     with doc.create(Section('Call to Order')):
-        president = input("President: ")
+        president = smart_input("President: ", default="Shepard Berry")
         prayer = input("Prayer: ")
 
         us_pledge = input("US Pledge: ")
         four_h_pledge = input("4-H Pledge: ")
         tx_pledge = input("TX Pledge: ")
-        
-        vp = input("Vice President: ")
+
+        vp = smart_input("Vice President: ", default="")
         inspiration_content = input("Summary of inspiration: on ")
-       
-        print("Roll Call: ") 
+
+        print("Roll Call: ")
         members_in_attendance = ""
         for index, member in enumerate(members, 1):
             here = input("--" + member + " ")
             if here != 'n':
                 # If last one, use oxford comma
                 if index == len(members):
-                    members_in_attendance += "and " + member 
+                    members_in_attendance += "and " + member
                 else:
                     members_in_attendance += member + ", "
         guests = input("New members or guests: ")
@@ -114,62 +128,51 @@ if __name__ == '__main__':
         else:
             introduction += "There were no new members or guests."
         doc.append(introduction)
-    
-    # Reports    
-    with doc.create(Section('Reports')):
-        
-        # Officer reports
-        with doc.create(Subsection('Officer')): 
-            secretary_report_approved = input("Old minutes approved: ")
-            
 
-            treasurer = input("Treasurer: ")
+    # Reports
+    with doc.create(Section('Reports')):
+
+        # Officer reports
+        with doc.create(Subsection('Officer')):
+            secretary_report_approved = input("Old minutes approved: ")
+
+
+            treasurer = smart_input("Treasurer: ", default="")
             balance = input("Treasury Balance: ")
             tr_notes = input("Notes from treasurery report: ")
-            
-            hlth_sfty_officer = input("Health and Safety Officer: ")
-            hlth_sfty_report = input("Health and Safety Topic: on")
 
-            cc_delegate = input("County Council Delegate: ")
+            hlth_sfty_officer = smart_input("Health and Safety Officer: ", default="")
+            hlth_sfty_report = input("Health and Safety Topic: on ")
+
+            cc_delegate = smart_input("County Council Delegate: ", default="")
             cc_report = input("County Council Report: such as ")
-            
+
             officer_reports = ""
             if secretary_report_approved != 'y':
                 officer_reports = "Correction to the minutes: " + secretary_report_approved
             else:
                 officer_reports = "The minutes were approved as read."
             officer_reports += " The financial report was given by %s. We have %s in the bank account. %s %s gave the Health and Safety Report on %s. %s gave an update on the County Council such as %s." % (treasurer, balance, tr_notes, hlth_sfty_officer, hlth_sfty_report, cc_delegate, cc_report)
-            doc.append(officer_reports)        
-        
-        # Project Reports 
+            doc.append(officer_reports)
+        # Project Reports
         with doc.create(Subsection('Projects')) as proj:
-            project_section = input("Any Projects?  ")
-            while project_section == 'y':
-                report(doc, subject="Project")
-                project_section = input("More Projects? ")
-        
+            askUser(doc, "Project")
         # Committee Reports
         with doc.create(Subsection('Committees')) as committee:
-            committee_section = input("Any Committees? ")
-            while committee_section == 'y':
-                report(doc, subject="Committee")
-                committee_section = input("More Committees? ")
-    
+            askUser(doc, "Committee")
     # Business
     with doc.create(Section("Business")):
-        
+
         # Old Business
         with doc.create(Subsection("Old Business")):
-            run_business(doc, type_of_business="Old Business") 
+            run_business(doc, type_of_business="Old Business")
         # New Business
         with doc.create(Subsection("New Business")):
-            run_business(doc, type_of_business="New Business") 
-    
+            run_business(doc, type_of_business="New Business")
+
     with doc.create(Section("Announcements")):
-        announcements(doc)        
-    
+        announcements(doc)
 
-    doc.generate_pdf(datetime.date.today().strftime("%B %d, %Y"), clean_tex=False)
+
     tex = doc.dumps()
-
-
+    doc.generate_tex("/home/"+datetime.date.today().strftime("%B %d, %Y"))
